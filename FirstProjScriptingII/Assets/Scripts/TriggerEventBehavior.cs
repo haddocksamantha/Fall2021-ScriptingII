92f401e9bb8818d2ccc32cd82d62f0f42d4e6d3c
Samantha Haddock <haddock.samanthalilly@gmail.com>
2021-09-01 12:22:23 -0600

    Unity

---
+++ FirstProjScriptingII/Assets/Scripts/TriggerEventBehavior.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEventBehavior : MonoBehaviour
+{
+private void onTriggerEnter(Collider other)
+{
+//triggerEnterEvent.Invoke();
+}
+}
