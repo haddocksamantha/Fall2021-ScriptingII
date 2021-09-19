18c9488f30707ce5a6da960fb28984a856eb5c45
Samantha Haddock <10911252@my.uvu.edu>
2021-09-18 21:10:19 -0600

    made successful moment in playercontroller

--- AntExterminatorz/Assets/Scripts/PlayerController.cs
+++ AntExterminatorz/Assets/Scripts/PlayerController.cs
@@ -1,27 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO.MemoryMappedFiles;
-using System.Numerics;
 using UnityEngine;
-using Vector3 = UnityEngine.Vector3;
 
 public class PlayerController : MonoBehaviour
 {
+    public float playerSpeed = 20.0f;
+    private float verticalInput;
 
+    // Update is called once per frame
     void Update()
     {
-        PlayerMove();
+       Movement(); 
     }
 
-    void PlayerMove()
+    void Movement()
     {
-    public float playerSpeed;
-    private float upAndDownInput;
-    //checks for keyboard input
-    upAndDownInput = Input.GetAxis("Vertical");
-    //moves player up and down
-    transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed * forwardInput);
-    
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * playerSpeed);
     }
-
 }
+
