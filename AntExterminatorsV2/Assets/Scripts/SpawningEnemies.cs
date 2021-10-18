ef5bdbe44dbc614f1bb5146727f4157f82aeef79
Samantha Haddock <59586234+haddocksamantha@users.noreply.github.com>
2021-10-17 21:25:14 -0600

    Worked on enemy spawning

--- AntExterminatorsV2/Assets/Scripts/SpawningEnemies.cs
+++ AntExterminatorsV2/Assets/Scripts/SpawningEnemies.cs
@@ -1,13 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class SpawningEnemies : MonoBehaviour
 {
+    public int numOfEnemies;
+
+    private int enemiesNeeded = 1;
+    private float enemyX;
+    private float enemyY;
+    private float enemyZ;
+    private float zRange;
+    private Vector3 enemyTransform;
+    
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemies();
+        numOfEnemies = 0;
     }
 
     // Update is called once per frame
@@ -19,10 +29,26 @@ public class SpawningEnemies : MonoBehaviour
     void AreThereEnemies()
     {
         //this will check for how many enemies are in the scene and call spawning method
+        if (numOfEnemies == 0)
+        {
+            SpawnEnemies();
+        } else if (numOfEnemies < enemiesNeeded)
+        {
+            SpawnEnemies();
+        }
+            
     }
 
     void SpawnEnemies()
     {
         //this will spawn enemies into the scene
+        EnemyTransformGenerator();
+        
+    }
+    
+    void EnemyTransformGenerator()
+    {
+        enemyZ = Random.Range(-zRange, zRange);
+        enemyTransform = new Vector3(enemyX, enemyY, enemyZ);
     }
 }
