9ef17b1fb7af1a61a5d8fb0a4cb9ca5fcdd552d2
Samantha Haddock <10911252@my.uvu.edu>
2021-10-24 17:51:39 -0600

    worked on script functionality

--- AntExterminatorsV2/Assets/Scripts/Bullet.cs
+++ AntExterminatorsV2/Assets/Scripts/Bullet.cs
@@ -1,27 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletDestroy : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
 	public GameObject bulletPrefab;
 	public GameObject enemyPrefab;
+	public float bulletSpeed;
 	
 	void Start()
 	{
 		enemyPrefab = GameObject.FindWithTag("Enemy");
-
+	}
+	
+	void Update()
+	{
+		BulletMovement();
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		Debug.Log("Enemy Hit!");
 		Destroy(enemyPrefab);
+		DestroyTheBullet();
 	}
 
     void DestroyTheBullet()
     {
         //this will destroy the bullet when it collides with the enemy 
+    }
+
+    void BulletMovement()
+    {
+	    transform.Translate(Vector3.right * Time.deltaTime * bulletSpeed );
     }
 
 }
--- AntExterminatorsV2/Assets/Scripts/BulletActivating.cs
+++ AntExterminatorsV2/Assets/Scripts/BulletActivating.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class BulletActivating : MonoBehaviour
 {
+    public GameObject bulletPrefab;
     // Update is called once per frame
+
+    void Start()
+    {
+        bulletPrefab = GameObject.FindWithTag("Bullet");
+    }
     void Update()
     {
-      //this will spawn bullets into the scene when player clicks the button  
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("Bullet Shot!)");
+            Instantiate(bulletPrefab,transform.position,transform.rotation);
+
+        }
+        //this will spawn bullets into the scene when player clicks the button  
     }
+    
+    
     
     
 }
