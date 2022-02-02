8dcc3dd74900c932b56d7b9f1d299e75f5f9081c
Samantha Haddock <59586234+haddocksamantha@users.noreply.github.com>
2022-02-01 18:03:02 -0700

    final push

--- AntExterminatorsV2/Assets/Scripts/Enemy Scripts/EnemyMovement.cs
+++ AntExterminatorsV2/Assets/Scripts/Enemy Scripts/EnemyMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private float enemySpeed = 2f;
+    private float enemySpeed = 3f;
     
     // Update is called once per frame
     void Update()
